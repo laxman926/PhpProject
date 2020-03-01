3cae1ec574daa43ac67c69cf2534e41045316710
Laxman <kumarlaxman926@gmail.com>
2020-03-01 11:49:11 -0500

    navbar

---
+++ navbar.php
@@ -0,0 +1,28 @@
+<header>
+<!-- Bootstrap link -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<nav class="navbar navbar-expand-lg navbar-dark " style="background-color:#232f3e">
+  <a class="navbar-brand" href="#">BookShop</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Products   </a>
+      </li>
+     
+   
+    </ul>
+   
+    
+      
+
+  </div>
+</nav>
+</header>
